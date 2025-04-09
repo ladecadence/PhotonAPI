10a7b09b9252e19da1287900a321cd4d0ac9ed27
David Pello <dvpg@tuta.io>
2025-04-09 15:58:43 +0200

    New /api/newwall endpoint with HTTP Auth

--- pkg/controllers/api.go
+++ pkg/controllers/api.go
@@ -1,11 +1,17 @@
 package controllers
 
 import (
+	"crypto/sha256"
+	"crypto/subtle"
 	"encoding/json"
+	"fmt"
+	"io"
+	"log"
 	"net/http"
 
 	"github.com/ladecadence/PhotonAPI/pkg/config"
 	"github.com/ladecadence/PhotonAPI/pkg/database"
+	"github.com/ladecadence/PhotonAPI/pkg/models"
 )
 
 var conf config.Config
@@ -67,4 +73,42 @@ func ApiGetWall(writer http.ResponseWriter, request *http.Request) {
 	writer.WriteHeader(http.StatusOK)
 	writer.Write(res)
 	writer.Write([]byte("\n"))
+}
+
+func ApiNewWall(writer http.ResponseWriter, request *http.Request) {
+	// check auth
+	username, password, ok := request.BasicAuth()
+	if ok {
+		// get username from DB
+		user, err := db.GetUser(username)
+		if err != nil {
+			// no such user
+			writer.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
+			http.Error(writer, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		// ok, we have a username, check password
+		passwordHash := fmt.Sprintf("%x", sha256.Sum256([]byte(password)))
+		passwordMatch := (subtle.ConstantTimeCompare([]byte(passwordHash), []byte(user.Password)) == 1)
+		if passwordMatch {
+			writer.WriteHeader(http.StatusOK)
+			reqBody, _ := io.ReadAll(request.Body)
+			request.Body.Close()
+			// try to create new wall
+			wall := &models.Wall{}
+			err := json.Unmarshal(reqBody, wall)
+			if err != nil {
+				log.Printf("❌ Error decoding body: %v", err.Error())
+			}
+			data, err := json.Marshal(wall)
+			writer.Write(data)
+		} else {
+			writer.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
+			http.Error(writer, "Unauthorized", http.StatusUnauthorized)
+		}
+
+	} else {
+		writer.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
+		http.Error(writer, "Unauthorized", http.StatusUnauthorized)
+	}
 }
--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -9,7 +9,8 @@ import (
 )
 
 func RegisterRoutes(db database.Database, config config.Config, router *http.ServeMux) {
-	router.HandleFunc("/api", controllers.ConfMiddleWare(db, config, controllers.ApiRoot))
-	router.HandleFunc("/api/walls", controllers.ConfMiddleWare(db, config, controllers.ApiGetWalls))
-	router.HandleFunc("/api/wall/{uid}", controllers.ConfMiddleWare(db, config, controllers.ApiGetWall))
+	router.HandleFunc("GET /api", controllers.ConfMiddleWare(db, config, controllers.ApiRoot))
+	router.HandleFunc("GET /api/walls", controllers.ConfMiddleWare(db, config, controllers.ApiGetWalls))
+	router.HandleFunc("GET /api/wall/{uid}", controllers.ConfMiddleWare(db, config, controllers.ApiGetWall))
+	router.HandleFunc("POST /api/newwall", controllers.ConfMiddleWare(db, config, controllers.ApiNewWall))
 }
