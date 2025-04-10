da5343de6b0c0f9d4ec4a549b0eced3301ebefd0
David Pello <dvpg@tuta.io>
2025-04-10 10:55:09 +0200

    Add problems api, small fixes and cleanup

--- pkg/controllers/api.go
+++ pkg/controllers/api.go
@@ -62,7 +62,7 @@ func ApiGetWalls(writer http.ResponseWriter, request *http.Request) {
 
 	if err != nil || walls == nil {
 		writer.WriteHeader(http.StatusNoContent)
-		writer.Write([]byte(`{}`))
+		writer.Write([]byte(`{}\n`))
 		return
 	}
 
@@ -79,14 +79,14 @@ func ApiGetWall(writer http.ResponseWriter, request *http.Request) {
 	uid := request.PathValue("uid")
 	if uid == "" {
 		writer.WriteHeader(http.StatusNoContent)
-		writer.Write([]byte(`{}`))
+		writer.Write([]byte(`{}\n`))
 		return
 	}
 	wall, err := db.GetWall(uid)
 
 	if err != nil {
 		writer.WriteHeader(http.StatusNoContent)
-		writer.Write([]byte(`{}`))
+		writer.Write([]byte(`{}\n`))
 		return
 	}
 
@@ -101,7 +101,6 @@ func ApiNewWall(writer http.ResponseWriter, request *http.Request) {
 	// check auth
 	authOk := CheckAuth(request)
 	if authOk {
-		writer.WriteHeader(http.StatusOK)
 		reqBody, _ := io.ReadAll(request.Body)
 		request.Body.Close()
 		// try to create new wall
@@ -109,17 +108,115 @@ func ApiNewWall(writer http.ResponseWriter, request *http.Request) {
 		err := json.Unmarshal(reqBody, &wall)
 		if err != nil {
 			log.Printf("❌ Error decoding body: %v", err.Error())
+			writer.WriteHeader(http.StatusBadRequest)
+			writer.Write([]byte("{}\n"))
+			return
 		}
 		err = db.UpsertWall(wall)
 		if err != nil {
 			writer.WriteHeader(http.StatusBadRequest)
-			writer.Write([]byte("{}"))
+			writer.Write([]byte("{}\n"))
 			return
 		}
 		data, err := json.Marshal(wall)
+		writer.WriteHeader(http.StatusOK)
+		writer.Write(data)
+		writer.Write([]byte("\n"))
+	} else {
+		writer.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
+		http.Error(writer, "Unauthorized", http.StatusUnauthorized)
+	}
+}
+
+func ApiGetProblems(writer http.ResponseWriter, request *http.Request) {
+
+	problems, err := db.GetProblems()
+
+	if err != nil || problems == nil {
+		writer.WriteHeader(http.StatusNoContent)
+		writer.Write([]byte(`{}\n`))
+		return
+	}
+
+	res, _ := json.Marshal(problems)
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+	writer.Write(res)
+	writer.Write([]byte("\n"))
+}
+
+func ApiGetProblem(writer http.ResponseWriter, request *http.Request) {
+	// get ID
+	uid := request.PathValue("uid")
+	if uid == "" {
+		writer.WriteHeader(http.StatusNoContent)
+		writer.Write([]byte(`{}\n`))
+		return
+	}
+	problem, err := db.GetProblem(uid)
+
+	if err != nil {
+		writer.WriteHeader(http.StatusNoContent)
+		writer.Write([]byte(`{}\n`))
+		return
+	}
+
+	res, _ := json.Marshal(problem)
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+	writer.Write(res)
+	writer.Write([]byte("\n"))
+}
+
+func ApiNewProblem(writer http.ResponseWriter, request *http.Request) {
+	// check auth
+	authOk := CheckAuth(request)
+	if authOk {
+		reqBody, _ := io.ReadAll(request.Body)
+		request.Body.Close()
+		// try to create new wall
+		problem := models.Problem{}
+		err := json.Unmarshal(reqBody, &problem)
+		if err != nil {
+			writer.WriteHeader(http.StatusBadRequest)
+			log.Printf("❌ Error decoding body: %v", err.Error())
+			writer.Write([]byte("{}\n"))
+			return
+		}
+		err = db.UpsertProblem(problem)
+		if err != nil {
+			writer.WriteHeader(http.StatusBadRequest)
+			writer.Write([]byte("{}\n"))
+			return
+		}
+		data, err := json.Marshal(problem)
+		writer.WriteHeader(http.StatusOK)
 		writer.Write(data)
 	} else {
 		writer.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
 		http.Error(writer, "Unauthorized", http.StatusUnauthorized)
 	}
+}
+
+func ApiGetWallProblems(writer http.ResponseWriter, request *http.Request) {
+	// get ID
+	wallid := request.PathValue("wallid")
+	if wallid == "" {
+		writer.WriteHeader(http.StatusNoContent)
+		writer.Write([]byte(`{}\n`))
+		return
+	}
+	problem, err := db.GetWallProblems(wallid)
+
+	if err != nil {
+		writer.WriteHeader(http.StatusNoContent)
+		writer.Write([]byte(`{}\n`))
+		return
+	}
+
+	res, _ := json.Marshal(problem)
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+	writer.Write(res)
+	writer.Write([]byte("\n"))
 }
