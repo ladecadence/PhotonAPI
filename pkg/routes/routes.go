43812a13dfdfc865e37996d87a75f62868622235
David Pello <dvpg@tuta.io>
2025-04-10 10:55:58 +0200

    Add problem routes

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -13,4 +13,8 @@ func RegisterRoutes(db database.Database, config config.Config, router *http.Ser
 	router.HandleFunc("GET /api/walls", controllers.ConfMiddleWare(db, config, controllers.ApiGetWalls))
 	router.HandleFunc("GET /api/wall/{uid}", controllers.ConfMiddleWare(db, config, controllers.ApiGetWall))
 	router.HandleFunc("POST /api/newwall", controllers.ConfMiddleWare(db, config, controllers.ApiNewWall))
+	router.HandleFunc("GET /api/problems", controllers.ConfMiddleWare(db, config, controllers.ApiGetProblems))
+	router.HandleFunc("GET /api/problem/{uid}", controllers.ConfMiddleWare(db, config, controllers.ApiGetProblem))
+	router.HandleFunc("POST /api/newproblem", controllers.ConfMiddleWare(db, config, controllers.ApiNewProblem))
+	router.HandleFunc("GET /api/wall/{walluid}/problems", controllers.ConfMiddleWare(db, config, controllers.ApiGetWallProblems))
 }
