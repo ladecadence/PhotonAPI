52f28ea9f176bd4e2bd243c480bc352e16589369
David Pello <zako@ladecadence.net>
2025-04-14 11:02:26 +0200

    Added routes for user signup, login, and logout (with tokens)

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -10,11 +10,17 @@ import (
 
 func RegisterRoutes(db database.Database, config config.Config, router *http.ServeMux) {
 	router.HandleFunc("GET /api", controllers.ConfMiddleWare(db, config, controllers.ApiRoot))
+	// walls
 	router.HandleFunc("GET /api/walls", controllers.ConfMiddleWare(db, config, controllers.ApiGetWalls))
 	router.HandleFunc("GET /api/wall/{uid}", controllers.ConfMiddleWare(db, config, controllers.ApiGetWall))
 	router.HandleFunc("POST /api/newwall", controllers.ConfMiddleWare(db, config, controllers.ApiNewWall))
+	// problems
 	router.HandleFunc("GET /api/problems", controllers.ConfMiddleWare(db, config, controllers.ApiGetProblems))
 	router.HandleFunc("GET /api/problem/{uid}", controllers.ConfMiddleWare(db, config, controllers.ApiGetProblem))
 	router.HandleFunc("POST /api/newproblem", controllers.ConfMiddleWare(db, config, controllers.ApiNewProblem))
 	router.HandleFunc("GET /api/wall/{walluid}/problems", controllers.ConfMiddleWare(db, config, controllers.ApiGetWallProblems))
+	// users
+	router.HandleFunc("POST /api/signup", controllers.ConfMiddleWare(db, config, controllers.ApiSignup))
+	router.HandleFunc("GET /api/login", controllers.ConfMiddleWare(db, config, controllers.ApiLogin))
+	router.HandleFunc("GET /api/logout", controllers.ConfMiddleWare(db, config, controllers.ApiLogout))
 }
