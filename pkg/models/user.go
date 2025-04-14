9215e12927dd203fe4aff2d5b6b6e5f286c60baa
David Pello <zako@ladecadence.net>
2025-04-14 11:01:43 +0200

    Updated User model for Token-based Auth

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -10,6 +10,8 @@ type UserRole int
 type User struct {
 	Name     string `gorm:"unique"`
 	Password string
-	Email    string
+	Email    string `gorm:"unique"`
 	Role     UserRole
+	Token    string
+	CSRF     string
 }
